ead96be8517b1e8a3e93c186a4b0689e08655910
Jacob M. Roufa <jacob.roufa@gmail.com>
2017-01-20 19:35:01 -0600

    add endpoints for retreiving scales by key and mode

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ Song Queue
 Run `npm install` followed by `npm start`, then visit http://localhost:8008 to view the application.
 
 This is intended to be run as a service somewhere for my personal use at band practice.
+
+## JSON endpoints
+
+`/scale/:key-:mode` returns an array `scale` with the notes of the requested scale, e.g. `/scale/ab-minor-pentatonic`, if available.
+
+`/scales/:key` returns an object of arrays with scales in the provided key for each of the available scales.
+
+### Available scales
+
++ major
++ minor
++ dorian
++ major pentatonic
++ minor pentatonic
--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const path = require('path');
 const express = require('express');
+const scale = require('tonal-scale');
 
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/js/lib', express.static(path.join(__dirname, 'bower_components')));
 
-app.listen(8008, function () {
+app.get('/scale/:key-:mode', (req, res) => {
+    const key = req.params.key;
+    const mode = req.params.mode.replace('-', ' ');
+
+    res.json({
+        scale: scale.get(mode, key)
+    });
+});
+
+app.get('/scales/:key', (req, res) => {
+    const key = req.params.key;
+    const modes = ['major', 'minor', 'dorian', 'major pentatonic', 'minor pentatonic'];
+    const scales = {};
+
+    modes.forEach((mode) => {
+        scales[mode] = scale.get(mode, key);
+    });
+
+    res.json({
+        scales: scales
+    });
+});
+
+app.listen(8008, () => {
     console.log('song-queue is being served at http://localhost:8008');
 });
